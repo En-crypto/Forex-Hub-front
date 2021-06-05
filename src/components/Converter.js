fe38c45035cdae6abf08710e8573142725446919
Haneenabonser <aabonser@gmail.com>
2021-06-05 22:56:46 +0300

    haneens bad

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,24 +1,71 @@
 import React from 'react';
-import ConverterCss from './Converter.css';
+import axios from 'axios';
+import { Form } from 'react-bootstrap';
 
- function Converter () {
-return (
-    <>
-    <h2>Currency Convert</h2>
-    <div>
-    <input type="number"/>
-    <select>
-        <option value="currency">Currency</option>
-    </select>
-    </div>
-    <span>||</span>
-    <div id="second">
-    <input type="number"/>
-    <select>
-        <option value="currency">Currency</option>
-    </select>
-    </div>
-    </>
-    )
+
+class Converter extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            currencyFrom: 'USD',
+            currencyTo: 'JOD',
+            amount: '1',
+            result: '0.71',
+            currencies: [],
+            
+        }
+    }
+    // https://api.exchangerate.host/convert?from=USD&to=EUR
+    getConverterData = async (e) => {
+        e.preventDefault();
+        this.setState({
+            currencyFrom: this.state.currencyFrom,
+            currencyTo: this.state.currencyTo
+        })
+        const resultData = await axios.get(`https://api.exchangerate.host/convert?from=${this.state.currencyFrom}&to=${this.state.currencyTo}`);
+        console.log(resultData.data);
+    }
+
+ 
+    getSymbols = async () => {
+        const selectData = await axios.get(`https://api.exchangerate.host/symbols`);
+        this.setState({
+            currencies: selectData.data.symbols.code,
+            //  currencies: selectData.data.map(item => {
+            //     return item.symbols.code;
+        })
+        console.log(this.state.currencies);
+    }
+
+
+    amountHandler = (event) => {
+        this.setState({
+            amount: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <Form onSubmit={this.getConverterData}>
+                    <h2>Currency Convert</h2>
+                    <input type="text" onChange={this.amountHandler} />
+                    <input type="submit" />
+
+                    <select  onChange={this.getSymbols}>
+                        {this.state.currencies.map(item=>{
+                            return<option></option>
+                        })}
+                    </select>
+                    <span>=</span>
+                    <select onChange={this.getSymbols}>
+                        <option value="currency">{this.state.currencyTo}</option>
+                    </select>
+                </Form>
+
+            </>
+        )
+    }
 }
 export default Converter;
